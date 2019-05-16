e5811b9835ab1dbb89bc7edfc766fbdeb8878833
MVCoder <1258235316@QQ.COM>
2019-05-17 02:25:00 +0800

    Initial commit

---
+++ app/src/main/java/media/ushow/as_video_player/MainActivity.java
@@ -0,0 +1,69 @@
+package media.ushow.as_video_player;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+    private Button forward_video_player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        forward_video_player = (Button) findViewById(R.id.forward_video_player);
+        forward_video_player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CP_EXTERNAL_STORAGE);
+                }else{
+                    joinToChangbaPlayer();
+                }
+            }
+        });
+    }
+
+    private void joinToChangbaPlayer(){
+        Intent intent = new Intent(MainActivity.this, ChangbaPlayerActivity.class);
+        startActivity(intent);
+    }
+
+    private final static int CP_EXTERNAL_STORAGE = 1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CP_EXTERNAL_STORAGE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                joinToChangbaPlayer();
+            }else{
+                Toast.makeText(this,"需要权限才能播放音视频",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
