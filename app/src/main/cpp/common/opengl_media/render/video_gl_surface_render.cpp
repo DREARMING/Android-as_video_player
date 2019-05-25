c9e0090d04fcc1b58cecc6040d29e0155d71980c
mvcoder <1258235316@qq.com>
2019-05-25 12:05:26 +0800

    更新一些 egl_core 笔记

--- app/src/main/cpp/common/opengl_media/render/video_gl_surface_render.cpp
+++ app/src/main/cpp/common/opengl_media/render/video_gl_surface_render.cpp
@@ -109,20 +109,29 @@ void VideoGLSurfaceRender::renderToView(GLuint texID) {
 	}
 
 	glUseProgram (mGLProgId);
+	//位置分别是：左下、右下、左上、右上
 	static const GLfloat _vertices[] = { -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f };
+	//绑定顶点数据到指定顶点属性：第一个参数是顶点属性在顶点shader的位置，然后分别是顶点的大小，类型，是否经常变化(涉及到选择何种缓冲区),每个顶点之间的偏移量（0代表紧密相连),
+	//顶点的数据
 	glVertexAttribPointer(mGLVertexCoords, 2, GL_FLOAT, 0, 0, _vertices);
+	//启用指定顶点属性
 	glEnableVertexAttribArray(mGLVertexCoords);
 //  todo: 这里有疑问，按照预想的应该是被注释掉的纹理坐标，但实际上是下面的正常工作。只有在往preview上去渲染时
 //  才这样，why?
 //	static const GLfloat texCoords[] = { 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f };
 	static const GLfloat texCoords[] = { 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+	//跟顶点坐标类似，这里是纹理坐标。
 	glVertexAttribPointer(mGLTextureCoords, 2, GL_FLOAT, 0, 0, texCoords);
 	glEnableVertexAttribArray(mGLTextureCoords);
 
 	/* Binding the input texture */
+	//激活纹理0
 	glActiveTexture(GL_TEXTURE0);
+	//绑定纹理0的纹理id
 	glBindTexture(GL_TEXTURE_2D, texID);
+	//将uniform的属性定位到位置0，本身是 纹理属性，所以就是对应上面的 GL_TEXTURE0
     glUniform1i(mGLUniformTexture, 0);
+    //
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 	glDisableVertexAttribArray(mGLVertexCoords);
@@ -142,6 +151,20 @@ void VideoGLSurfaceRender::renderToViewWithAutoFill(GLuint texID, int screenWidt
 	float viewAspectRatio = (float)screenHeight / (float)screenWidth;
 	float xOffset = 0.0f;
 	float yOffset = 0.0f;
+	/**
+	 * 这里的算法感觉有问题
+	 *
+	 * 下面是自己的理解算法：维持视频的比例算法
+	 * 当 textureAspectRatio > viewAspectRatio 时，说明图片的高/宽 > view的高/宽，
+	 * 也就是说：如果宽相等的话，图片的高将会超出界面。所以为了能够正确显示，应该假设高==view的高，将宽进行等比缩放，视频的比例将不会发生变化。
+	 * 这种假设是基于图片的高> view的高的，如果view的高大于图片的高呢？将会放大，会失真。那么仍需要处理一下这种情况，将图片置于view的中心。
+	 *
+	 * 当textureAspectRation < viewAspectRatio时，说明图片的高/宽 < view的高/宽，
+	 * 也就说，如果高相等，图片的宽将会 > view的宽，所以为了能够按照比例正常显示，应该假设宽==view的宽，高度进行等比缩放。视频的比例将不会发生变化
+	 * 这种假设是基于图片的宽>view的宽，否则将会宽度变宽，从而失真。那么就需要将图片放置于view中心了
+	 *
+	 */
+
 	if(textureAspectRatio > viewAspectRatio){
 		//Update Y Offset
 		int expectedHeight = (int)((float)texHeight*screenWidth/(float)texWidth+0.5f);
@@ -155,20 +178,28 @@ void VideoGLSurfaceRender::renderToViewWithAutoFill(GLuint texID, int screenWidt
 	}
 
 	glUseProgram(mGLProgId);
+    //位置分别是：左下、右下、左上、右上
 	static const GLfloat _vertices[] = { -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f };
+    //绑定顶点数据到指定顶点属性：第一个参数是顶点属性在顶点shader的位置，然后分别是顶点的大小，类型，是否经常变化(涉及到选择何种缓冲区),每个顶点之间的偏移量（0代表紧密相连),
+    //顶点的数据
 	glVertexAttribPointer(mGLVertexCoords, 2, GL_FLOAT, 0, 0, _vertices);
 	glEnableVertexAttribArray(mGLVertexCoords);
     static const GLfloat texCoords[] = { xOffset, (GLfloat)(1.0 - yOffset), (GLfloat)(1.0 - xOffset), (GLfloat)(1.0 - yOffset), xOffset, yOffset,
                                          (GLfloat)(1.0 - xOffset), yOffset };
 	glVertexAttribPointer(mGLTextureCoords, 2, GL_FLOAT, 0, 0, texCoords);
+    //启用指定顶点属性
 	glEnableVertexAttribArray(mGLTextureCoords);
 
 	/* Binding the input texture */
 	glActiveTexture (GL_TEXTURE0);
+    //激活纹理0
 	glBindTexture(GL_TEXTURE_2D, texID);
+    //将uniform的属性定位到位置0，本身是 纹理属性，所以就是对应上面的 GL_TEXTURE0
 	glUniform1i(mGLUniformTexture, 0);
+	//绘制、一张图片是矩形，所以画4个点
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+	//关闭顶点、纹理属性和解绑纹理0 -- 因为OpenGL上下文就是一个巨大的状态机，这里设置了状态、用完就要清空
 	glDisableVertexAttribArray(mGLVertexCoords);
 	glDisableVertexAttribArray(mGLTextureCoords);
 	glBindTexture(GL_TEXTURE_2D, 0);
